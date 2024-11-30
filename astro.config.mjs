bcda6e12c84d0c5f93d662481caf02b0c2596106
Lautaro Acosta Quintana <acostaquintanalautaro@gmail.com>
2024-11-30 01:37:40 -0300

    commit 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,19 +4,24 @@ import { defineConfig, envField } from "astro/config";
 
 import react from "@astrojs/react";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react()],
-	experimental: {
-		env: {
-			schema: {
-				API_URL: envField.string({
-					context: "client",
-					access: "public",
-					optional: true,
-				}),
-			},
-		},
-	},
-});
+  integrations: [react()],
 
+  experimental: {
+      env: {
+          schema: {
+              API_URL: envField.string({
+                  context: "client",
+                  access: "public",
+                  optional: true,
+              }),
+          },
+      },
+	},
+
+  output: "server",
+  adapter: netlify(),
+});
